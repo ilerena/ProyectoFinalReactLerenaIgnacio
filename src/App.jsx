a94b8f3d324f09ac75407e5701257afb5c564788
ilerena <ignaciolerena@hotmail.com>
2022-11-09 23:55:17 -0300

    Eliminación HOME - correcion para entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import Home from "./pages/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import ItemListContainer from "./components/ItemListContainer";
+import NavBar from "./components/NavBar";
+import SecondBar from "./components/SecondBar";
+import Body from "./components/Body";
 
 const App = () => {
-  return <Home />;
-};
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <SecondBar />
+            <Routes>
+                <Route path='/' element={<Body />} />
+                <Route path='/category/:idCategory' element={<ItemListContainer />} />
+                <Route path='/item/:idItem' element={<ItemDetailContainer />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
 export default App;
